b3ddb130a0f508663b46520063fc7f4a5a1ec572
Da-Hahni <simonhahn007@gmail.com>
2026-01-13 17:58:51 +0100

    UI überarbeitet + UI, main baseplate u. Generator zusammengeführt

--- civitas/Scripts/add_box_button.gd
+++
@@ -1,8 +0,0 @@
-extends Button
-
-@onready var right_area = $"../HBoxContainer/RightArea"
-@onready var panel = $".."
-
-func _on_pressed():
-	print("Button gedrückt")
-	panel.position += Vector2(-100, 0)
