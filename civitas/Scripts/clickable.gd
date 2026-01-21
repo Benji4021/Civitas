cdf4835d02c44444a99a3b6ecd217e482c6a9158
Benji4021 <114071368+Benji4021@users.noreply.github.com>
2026-01-21 11:58:12 +0100

    Terrain nur einmal clickbar
    Terrain kann nicht mehrmals abgebaut werden

--- civitas/Scripts/clickable.gd
+++ civitas/Scripts/clickable.gd
@@ -6,5 +6,6 @@ func _ready():
 
 func _input_event(_v, event, _i):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		input_pickable = false
 		print("Object clicked!")
 		SignalBus.terrain_clicked.emit(get_parent())
