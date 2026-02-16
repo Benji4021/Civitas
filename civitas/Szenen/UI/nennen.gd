b9757254c65758c985b295388183f63d966bfa33
vik <v.weiss@htlkrems.at>
2026-02-16 10:18:15 +0100

    Bennenen alter ahh

---
+++ civitas/Szenen/UI/nennen.gd
@@ -0,0 +1,43 @@
+extends Control
+
+@onready var title_label = $Panel/TextureRect/Label
+@onready var name_input = $Panel/TextureRect/LineEdit
+@onready var confirm_button = $Button
+
+var phase := "ruler"
+
+func start_ruler_naming():
+	Globals.input_locked = true
+	phase = "ruler"
+	title_label.text = "Name deines Königs:"
+	name_input.text = ""
+	visible = true
+	name_input.grab_focus()
+
+func start_kingdom_naming():
+	Globals.input_locked = true
+	phase = "kingdom"
+	title_label.text = "Name deines Königreichs:"
+	name_input.text = ""
+	visible = true
+	name_input.grab_focus()
+
+
+func _on_ConfirmButton_pressed():
+	var entered_name : String = name_input.text.strip_edges()
+	if entered_name == "":
+		return
+
+	if phase == "ruler":
+		Globals.input_locked = true
+		Globals.ruler_name = entered_name
+		print("König heißt jetzt:", Globals.ruler_name)
+
+		start_kingdom_naming()
+
+	elif phase == "kingdom":
+		Globals.kingdom_name = entered_name
+		print("Königreich heißt jetzt:", Globals.kingdom_name)
+
+		visible = false
+		Globals.input_locked = false
---
+++ civitas/Szenen/benennung.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+@onready var benennung = $CanvasLayer/Benennung
+
+var ruler_named := false
+var kingdom_named := false
+
+func _ready():
+	if not ruler_named and not kingdom_named:
+		benennung.visible = true
+		benennung.start_ruler_naming()
+		Globals.input_locked = true
+	else:
+		Globals.input_locked = false
