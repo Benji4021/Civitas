a7deeb77c183461ef45dc4cb509d5bdc94fed054
Da-Hahni <simonhahn007@gmail.com>
2026-04-17 19:20:23 +0200

    Haus platzierung Ressourcen check +  Fehlende Ressourcen UI

--- civitas/Scripts/Global/TimeManager.gd
+++ civitas/Scripts/Global/TimeManager.gd
@@ -4,9 +4,6 @@ var current_day: int = 1
 var minutes_per_day: int = 1
 var seconds_passed: float = 0.0
 
-var wood_to_stone = Vector2i(1, 1) # e.g. 1 wood = 3 stone
-var stone_to_wood = Vector2i(1, 1)
-
 signal day_changed(new_day)
 
 func _process(delta):
@@ -15,19 +12,8 @@ func _process(delta):
 	if seconds_passed >= minutes_per_day * 30:
 		seconds_passed = 0
 		current_day += 1
-		generate_new_trades()
 		emit_signal("day_changed", current_day)
 		print("New Day:", current_day)
 
 func get_day_progress() -> float:
 	return seconds_passed / (minutes_per_day * 60.0)
-	
-func generate_new_trades():
-	wood_to_stone = Vector2i(
-		randi_range(1, 5), # wood amount
-		randi_range(1, 5)  # stone amount
-	)
-	stone_to_wood = Vector2i(
-		randi_range(1, 5),
-		randi_range(1, 5)
-	)
---
+++ civitas/Szenen/Buildings/lumbermill.gd
@@ -0,0 +1,9 @@
+extends Building
+
+func _ready():
+	super._ready()
+	cost_wood = 10
+	cost_stone = 10
+	cost_gold = 10
+	cost_people = 10
+	BuildingType = Buildings.Lumbermill
